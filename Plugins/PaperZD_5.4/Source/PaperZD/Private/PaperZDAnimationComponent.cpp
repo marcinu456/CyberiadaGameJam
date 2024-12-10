944cff826fd2b06474681a06f3a4938cee8f4604
marcinu456 <marcinu456@gmail.com>
2024-12-10 19:06:46 +0100

    [Marcin] Added Plugin to project

---
+++ Plugins/PaperZD_5.4/Source/PaperZD/Private/PaperZDAnimationComponent.cpp
@@ -0,0 +1,141 @@
+// Copyright 2017 ~ 2022 Critical Failure Studio Ltd. All rights reserved.
+
+#include "PaperZDAnimationComponent.h"
+#include "PaperZDAnimInstance.h"
+#include "Components/PrimitiveComponent.h"
+#include "PaperZDCustomVersion.h"
+#include "Engine/World.h"
+
+#if ZD_VERSION_INLINED_CPP_SUPPORT
+#include UE_INLINE_GENERATED_CPP_BY_NAME(PaperZDAnimationComponent)
+#endif
+
+// Sets default values for this component's properties
+UPaperZDAnimationComponent::UPaperZDAnimationComponent()
+	: AnimInstanceClass(nullptr)
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	// ...
+}
+
+void UPaperZDAnimationComponent::PostLoad()
+{
+	Super::PostLoad();
+
+#if WITH_EDITOR
+	//Apply the patches to different versions.
+	//
+	//This extra check fixes an edge case with 'blueprint spawned' components. For whatever reason they call 'PostLoad' always.
+	//They might return a wrong 'LinkerVersion' when loading subsequent maps due to LinkerVersions checking the outermost instead of the object normally, which can be the world.
+	//Hence why we make sure we aren't on a 'game world' while applying the fixes.
+	const UWorld* World = GetWorld();
+	if (!World || !World->IsGameWorld())
+	{
+		const int32 ZDVersion = GetLinkerCustomVersion(FPaperZDCustomVersion::GUID);
+		if (ZDVersion < FPaperZDCustomVersion::AddedCustomComponentPicker)
+		{
+			//Use the new component ref type which uses the new and improved picker
+			RenderComponentRef.ComponentProperty = RenderComponent_DEPRECATED.ComponentProperty;
+			RenderComponentRef.PathToComponent = RenderComponent_DEPRECATED.PathToComponent;
+			RenderComponentRef.OtherActor = nullptr;
+			RenderComponentRef.OverrideComponent = nullptr;
+		}
+	}
+#endif
+}
+
+void UPaperZDAnimationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Create a fresh AnimInstance object
+	CreateAnimInstance();
+}
+
+void UPaperZDAnimationComponent::Serialize(FArchive& Ar)
+{
+	Super::Serialize(Ar);
+	Ar.UsingCustomVersion(FPaperZDCustomVersion::GUID);
+}
+
+void UPaperZDAnimationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	//Need to update the AnimInstance
+	if (AnimInstance)
+	{
+		AnimInstance->Tick(DeltaTime);
+	}
+}
+
+AActor* UPaperZDAnimationComponent::GetOwningActor() const
+{
+	return GetOwner();
+}
+
+UPrimitiveComponent* UPaperZDAnimationComponent::GetRenderComponent() const
+{
+	return Cast<UPrimitiveComponent>(RenderComponentRef.GetComponent(GetOwner()));
+}
+
+TSubclassOf<UPaperZDAnimInstance> UPaperZDAnimationComponent::GetSequencerAnimInstanceClass() const
+{
+	return AnimInstanceClass;
+}
+
+UPaperZDAnimInstance* UPaperZDAnimationComponent::GetSequencerAnimInstance()
+{
+	return GetOrCreateAnimInstance();
+}
+
+UPaperZDAnimInstance* UPaperZDAnimationComponent::GetOrCreateAnimInstance()
+{
+	if (!AnimInstance || !AnimInstance->GetPlayer())
+	{
+		CreateAnimInstance();
+	}
+
+	return AnimInstance;
+}
+
+void UPaperZDAnimationComponent::CreateAnimInstance()
+{
+	if (AnimInstanceClass)
+	{
+		AnimInstance = NewObject<UPaperZDAnimInstance>(this, AnimInstanceClass);
+		AnimInstance->Init(this);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No animation class defined on '%s', cannot create instance."), *GetName());
+	}
+}
+
+void UPaperZDAnimationComponent::InitRenderComponent(UPrimitiveComponent* InRenderComponent)
+{
+	const FString PathToComponent = *InRenderComponent->GetPathName(GetOwner());
+	RenderComponentRef.PathToComponent = PathToComponent;
+
+	//We need to also set the old deprecated component, because if this call was used on a constructor to set the correct path
+	//the PostLoad implementation will then override the new RenderComponent data with the old one, during the version upgrade process.
+	//If this hasn't been setup by that point, it will effectively clear the RenderComponentRef during upgrade.
+	RenderComponent_DEPRECATED.PathToComponent = PathToComponent;
+}
+
+void UPaperZDAnimationComponent::SetAnimInstanceClass(TSubclassOf<UPaperZDAnimInstance> InAnimInstanceClass)
+{
+	AnimInstanceClass = InAnimInstanceClass;
+
+	//Potentially re-create the anim instance, only if we already initialized the AnimInstance
+	AnimInstance = nullptr;
+	CreateAnimInstance();
+}
+
+void UPaperZDAnimationComponent::InitAnimInstanceClass(TSubclassOf<UPaperZDAnimInstance> InAnimInstanceClass)
+{
+	AnimInstanceClass = InAnimInstanceClass;
+	AnimInstance = nullptr;
+}
